f4ffd12d75a7c344471c4a9ce7dc74c13b9a0d6a
laszloekovacs <laszloekovacs@gmail.com>
2022-03-23 10:07:51 +0100

    created initial funcitonality

---
+++ main.js
@@ -0,0 +1,61 @@
+const taskDescription = document.getElementById('taskDescription');
+const taskSubmit = document.getElementById('taskSubmit');
+const taskReset = document.getElementById('taskReset');
+const tasks = document.getElementById('tasks');
+
+const isLogging = true;
+
+const jslog = (text) => {
+	if (!isLogging) return;
+
+	console.log(text);
+};
+
+const addNewEntry = (text, targetElem) => {
+	// create the list element itself
+	const task = document.createElement('li');
+
+	// create a p and a checkbox
+	const paragraph = document.createElement('p');
+	const checkbox = document.createElement('input');
+
+	// fill in the content
+	paragraph.innerText = text;
+	checkbox.setAttribute('type', 'checkbox');
+
+	// assemble the final entry
+	task.append(paragraph);
+	task.append(checkbox);
+
+	targetElem.prepend(task);
+};
+
+// fired when pressing enter or the add button
+const handleEnter = (event) => {
+	if (event.code === 'Enter') {
+		addNewEntry(taskDescription.value, tasks);
+		handleReset();
+	}
+};
+
+// reset button handler
+const handleReset = () => {
+	jslog('reset');
+
+	taskDescription.value = '';
+};
+
+/*
+ * assign event handlers
+ */
+taskDescription.addEventListener('keydown', (e) => {
+	handleEnter(e);
+});
+
+taskSubmit.addEventListener('click', (e) => {
+	handleEnter(e);
+});
+
+taskReset.addEventListener('click', (e) => {
+	handleReset();
+});
